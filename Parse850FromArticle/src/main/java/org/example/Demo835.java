0cd697dc4931a3589fb36c368f59396abc5657c4
david.lloyd <dlloyd@arka.org>
2025-08-15 17:12:33 -0600

    Fixing recommendation by IDE

--- Parse850FromArticle/src/main/java/org/example/Demo835.java
+++ Parse850FromArticle/src/main/java/org/example/Demo835.java
@@ -2,7 +2,10 @@ package org.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.XML.X12_835_Parser;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -10,18 +13,30 @@ import java.nio.file.Paths;
 public class Demo835 {
     public static void main(String[] args) throws Exception {
         // Load an 835 EDI message from resources
-        byte[] ediBytes = Files.readAllBytes(
-                Paths.get(Demo835.class.getClassLoader().getResource("input835.edi").toURI())
-        );
-        String originalEDIString = Files.readString(Paths.get(Demo835.class.getClassLoader().getResource("input835.edi").toURI()));
-        System.out.println("Original      835 EDI:\n" + originalEDIString);
+        try {
+            byte[] ediBytes = Files.readAllBytes(
+                    Paths.get(Demo835.class.getClassLoader().getResource("input835.edi").toURI())
+            );
+            String originalEDIString = Files.readString(Paths.get(Demo835.class.getClassLoader().getResource("input835.edi").toURI()));
+            System.out.println("Original      835 EDI:\n" + originalEDIString);
 
-        // EDI -> XML
-        String xml = X12_835_Parser.parseEDI(ediBytes);
-        System.out.println("Parsed 835 XML:\n" + xml);
+            // EDI -> XML
+            String xml = X12_835_Parser.parseEDI(ediBytes);
+            System.out.println("Parsed 835 XML:\n" + xml);
 
-        // XML -> EDI
-        String edi = X12_835_Parser.xmlToEDI(xml);
-        System.out.println("Re-serialized 835 EDI:\n" + edi);
+            // XML -> EDI
+            String edi = X12_835_Parser.xmlToEDI(xml);
+            System.out.println("Re-serialized 835 EDI:\n" + edi);
+        } catch (IOException e) {
+            log.error("IOException Error reading EDI file: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            log.error("URISyntaxException Error reading EDI file: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            log.error("SAXException Error reading EDI file: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+        }
     }
 }
